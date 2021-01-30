cf47be25376d5b5896a7c74a6c462e8c14113763
Claude Lu <claudelu@Claudes-MacBook-Pro.local>
2021-01-29 21:20:28 -0700

    added form and form validation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from SearchForm import SearchForm
 
 app = Flask(__name__)
 
@@ -6,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/displayTweets', methods=['GET', 'POST'])
+def displayTweets():
+    form = SearchForm(request.form)
+    if request.method == 'POST' and form.validate():
+        twitterUser = form.username.data
+        companyName = form.company.data
+        return 'It was validated'
+    return 'Wrong'
 if __name__ == '__main__':
     app.run(debug=True)
